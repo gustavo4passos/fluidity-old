c7d28652293bd6438bdbed4a428755d419cc6ca8
gustavo4passos <gustavo4passos@gmail.com>
2019-10-18 22:48:07 -0300

    Adds cuda build generators to CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,53 @@
+#include "utils/logger.h"
 #include "renderer/shader.h"
 #include "renderer/window.h"
+#include "simulation/particleSystem.h"
+
+extern "C" void cudaInit(int argc, char **args);
+
+void update_particle_system(ParticleSystem* p)
+{
+    p->setIterations(1);
+    p->setDamping(1.f);
+    p->setGravity(0.0003f);
+    p->setCollideSpring(.5f);
+    p->setCollideDamping(0.02f);
+    p->setCollideShear(0.1f);
+    p->setCollideAttraction(0.0f);
+    p->update(0.5f);
+}
 
 int main(int argc, char* args[]) 
 {
+    Window window = Window("OpenGL Fluid Rendering", 800, 600, 4, 5, true, false);
+    if(!window.Init()) 
+    {
+        LOG_ERROR("Unable to create window..");
+        return 0;
+    }
+    else LOG_WARNING("Window successfully created.");
+
+    cudaInit(argc, args);
+    ParticleSystem* particleSystem = new ParticleSystem(16000, {200, 200, 200}, true);
+    particleSystem->reset(ParticleSystem::CONFIG_GRID);
+
+    bool running = true;
+    while(running) 
+    {
+        SDL_Event e;
+        while(SDL_PollEvent(&e)) 
+        {
+            if(e.type == SDL_QUIT || (e.type == SDL_KEYUP && e.key.keysym.sym  == SDLK_ESCAPE))
+            {
+                running = false;
+            }
+        }
+
+        update_particle_system(particleSystem);
+        particleSystem->dumpParticles(0, 1);
+        glClearColor(.4f, .4f, .4f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        window.Swap();
+    }
     return 0;
 }
--- src/renderer/shader.h
+++ src/renderer/shader.h
@@ -22,13 +22,13 @@ public:
 	void Unbind();
 
 	// Set uniforms
-	void SetInt(const char* name, GLint v0);
-	void SetUniform1f(const char* name, GLfloat v0);
-	void SetUniform2f(const char* name, GLfloat v0, GLfloat v1);
-	void SetUniform3f(const char* name, GLfloat v0, GLfloat v1, GLfloat v2);
-	void SetUniform4f(const char* name, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
-	void SetUniform3fv(const char* name, GLfloat* v0);
-	void SetUniformMat4(const char* name, const void* data);
+	void SetInt        (const char* name, GLint v0										);
+	void SetUniform1f  (const char* name, GLfloat v0									);
+	void SetUniform2f  (const char* name, GLfloat v0, GLfloat v1						);
+	void SetUniform3f  (const char* name, GLfloat v0, GLfloat v1, GLfloat v2			);
+	void SetUniform4f  (const char* name, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+	void SetUniform3fv (const char* name, GLfloat* v0									);
+	void SetUniformMat4(const char* name, const void* data								);
 
 	// Only for debug purposes
 	void PrintActiveAttributes() const;
